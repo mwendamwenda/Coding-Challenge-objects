61d3fa45152209e8e321d5556c5b2d9a7ed320f7
mwendamwenda <benmwenda83@gmail.com>
2022-11-15 07:04:21 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('noma sana')
+'use strict';
+const mark = {
+    fullName : 'Mark Miller',
+    mass : 78,
+    height : 1.69,
+
+     calBMI : function(){
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+     }
+};
+const john = {
+    fullName : 'John Smith',
+    mass : 92,
+    height : 1.95,
+
+    calBMI : function(){
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+
+};
+mark.calBMI();
+console.log(mark.bmi);
+john.calBMI();
+console.log(john.bmi);
+
+if(mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+}else if(john.bmi > mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
